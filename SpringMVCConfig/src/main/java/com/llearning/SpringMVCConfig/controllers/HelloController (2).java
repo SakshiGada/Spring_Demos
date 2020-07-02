3c611a5bd657316231c298ab23a63c2c0167dab2
SakshiGada <sakshigada11@gmail.com>
2020-07-02 13:22:02 +0530

    inital commit

---
+++ SpringMVCConfig/src/main/java/com/llearning/SpringMVCConfig/controllers/HelloController (2).java
@@ -0,0 +1,20 @@
+package com.llearning.SpringMVCConfig.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController 
+{
+	@RequestMapping("/")
+	public String hello()
+	{
+		return "index";
+	}
+	
+	@RequestMapping("/demo")
+	public String greet()
+	{
+		return "demo";
+	}
+}
